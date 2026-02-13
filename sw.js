41df31fc583e88edb346f11e050a5aac543bbd3a
str-gglr <mark.beluscsak@gmail.com>
2026-02-13 22:39:28 +0100

    local opencv

--- sw.js
+++ sw.js
@@ -1,10 +1,19 @@
 const CACHE_NAME = 'sigil-scanner-auto-v1';
-const ASSETS = [ './', './index.html', './manifest.json' ];
+const ASSETS = [ 
+  './', 
+  './index.html', 
+  './manifest.json',
+  './opencv.js'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
